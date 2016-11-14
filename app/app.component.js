8bdbc7e82ca79114748f31e1667b064b630286b5
gguo <gguo@movoto.com>
2016-11-14 13:35:37 +0800

    finished playing

--- app/app.component.js
+++ app/app.component.js
@@ -9,8 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var hero_1 = require('./hero');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = "Tour of Heros";
+        this.heroes = [
+            new hero_1.Hero(1, 'Windstorm'),
+            new hero_1.Hero(13, 'Bombasto'),
+            new hero_1.Hero(15, 'Magneta'),
+            new hero_1.Hero(20, 'Tornado')
+        ];
+        this.myHero = this.heroes[0];
     }
     AppComponent = __decorate([
         core_1.Component({
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
+
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = "Tour of Heros";
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
+}
